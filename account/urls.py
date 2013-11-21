07ed09ee6f14f1d932b8f25b1d4e8d178721491d
Konrad Blaszkiewicz <konrad.blaszkiewicz@gmail.com>
2013-11-21 15:04:52 +0100

    add register url

--- account/urls.py
+++ account/urls.py
@@ -10,6 +10,7 @@ from account.views import SettingsView
 
 urlpatterns = patterns("",
     url(r"^signup/$", SignupView.as_view(), name="account_signup"),
+    url(r"^register/$", SignupView.as_view(), name="account_signup"),
     url(r"^login/$", LoginView.as_view(), name="account_login"),
     url(r"^logout/$", LogoutView.as_view(), name="account_logout"),
     url(r"^confirm_email/(?P<key>\w+)/$", ConfirmEmailView.as_view(), name="account_confirm_email"),
